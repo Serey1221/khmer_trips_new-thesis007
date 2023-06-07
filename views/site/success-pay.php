5efecd29deb4cba0d30de405f42ace06a9c2f209
Serey <sereysrout93@gmail.com>
2023-06-07 11:18:41 +0700

    Update view btn

--- views/site/success-pay.php
+++ views/site/success-pay.php
@@ -10,7 +10,16 @@ use yii\helpers\Url;
             <h3>Dear, Customer</h3>
             <p style="font-size:20px;color:#5C5C5C;">Thank you for choosing our company. We have received your full payment and have sent the receipt to your email already. We wish you have a pleasant day.</p>
             <a href="<?= Yii::$app->homeUrl ?>" class="btn btn-primary">Back Home</a>
+<<<<<<< HEAD
 
+=======
+            <?php
+            $booking = Yii::$app->request->get('booking');
+            if (!empty($booking)) {
+                echo \yii\helpers\Html::a('View My Booking', ['user/view-booking', 'code' => $booking], ['class' => 'btn btn-info ml-3']);
+            }
+            ?>
+>>>>>>> 4425128 (add button view my booking)
             <br><br>
         </div>
         <div class="col-lg-5">
